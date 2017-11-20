d64506c4f3255d199b23e0425fb807154145b2f8
Cuddlybear <yche@github.com>
2017-11-20 10:35:54 -0500

    change of hello
    add method

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@ public class Hello{
 
       public static void main(String[] args){
           System.out.println("hello");
+      public static void printJapanese(){
+          System.out.println("山")
+      }
       }
 }
